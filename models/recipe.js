6c569e6ad8047faf6941adbd9e640c49b481892d
Mac <mac@Macs-MacBook-Pro.local>
2022-01-14 04:03:08 +0530

    initial base setup

---
+++ models/recipe.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Recipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Recipe.init({
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    ingredients: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    direction: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }
+  }, {
+    sequelize,
+    modelName: 'Recipe',
+  });
+  Recipe.associate = function(models) {
+    Recipe.belongsTo(models.User, { foreignKey: 'userId' });
+  };
+  return Recipe;
+};
